f145e5f5e15967d32b8241ae91b4b5098b6e4ae5
Reggie Gulle <reggie.gulle@gmail.com>
2015-09-22 17:34:56 +1000

    Styling rendered with owl-carousel
    The styling of the app at this juncture was made with owl-carousel.

--- js/carousel.menu.js
+++ js/carousel.menu.js
@@ -20,7 +20,6 @@ $(document).ready(function(){
 			}
 	});
 	if (week_order === 'DESC'){
-		//var descPos = ($('#weeks_carousel_menu li').length) - 1;
 		var descPos = $('#weeks_carousel_menu li').index( $('#weeks_carousel_menu li').last());
 		owl_weeks_carousel_menu.trigger('to.owl.carousel', [parseInt(descPos)]);
 	}
@@ -47,48 +46,6 @@ $(document).ready(function(){
 				}
 			}
 	});
-	
-	while($('#videos_carousel li').length > 0){
-		owl_vids_car.trigger('remove.owl.carousel', [0]);
-	}
-	
-	var vids_car_len = $('#videos_list li').length;
-	for(n=(vids_car_len); n > 0; n--){
-		var vid_car_item = $('<div>');
-		var vid_car_img = $('#videos_list li').eq(n-1).find('img').clone();
-		vid_car_img.appendTo(vid_car_item);
-		var vid_car_datePub_data = $('#videos_list li').eq(n-1).find('h5').text();
-		var vid_car_datePub = $('<p>');
-		vid_car_datePub.html(vid_car_datePub_data);
-		vid_car_datePub.appendTo(vid_car_item);
-		var vid_car_title_data = $('#videos_list li').eq(n-1).find('h3').text();
-		var vid_car_title = $('<h5>');
-		vid_car_title.html(vid_car_title_data);
-		vid_car_title.appendTo(vid_car_item);
-		owl_vids_car.trigger('add.owl.carousel', [vid_car_item, 0]);
-	}
-	owl_vids_car.trigger('refresh.owl.carousel');
-	
-	$("#videos_carousel li").each(function(){
-		
-		$(this).css({
-			'height' : $('#videos_carousel').height() + 'px'
-		});
-		
-		
-		var srcUrl = $(this).find('img').attr('src');
-		var imgContainer = $(this);
-		var imgIndex = $(this).index();
-		//console.log(imgIndex);
-		var imgRegex = /https:\/\/i3.ytimg.com\/vi\/([\w-]{11})\/mqdefault.jpg/;
-		//get the videoid from each li item
-		var vidImgDat = $(this).find('img').attr('src');
-		var vidId = vidImgDat.match(imgRegex)[1];
-		//play the video on the player
-		$(this).on("click", "img", function(){
-			player.loadVideoById(vidId);
-		});
-	});
 
 	$('#videos_list').on('mh370.vidListRender', function(){
 		
@@ -100,17 +57,15 @@ $(document).ready(function(){
 		
 		var vids_car_len = $('#videos_list li').length;
 		for(n=(vids_car_len); n > 0; n--){
-			var vid_car_item = $('<div>');
-			var vid_car_img = $('#videos_list li').eq(n-1).find('img').clone();
-			vid_car_img.appendTo(vid_car_item);
+			var vid_car_item = '<div data-index="';
+			vid_car_item += (n-1) + '">';
+			var vid_car_img = $('#videos_list li').eq(n-1).find('img').prop('outerHTML');
+			vid_car_item += vid_car_img;
 			var vid_car_datePub_data = $('#videos_list li').eq(n-1).find('h5').text();
-			var vid_car_datePub = $('<p>');
-			vid_car_datePub.html(vid_car_datePub_data);
-			vid_car_datePub.appendTo(vid_car_item);
+			vid_car_item += '<p>' + vid_car_datePub_data + '</p>';
 			var vid_car_title_data = $('#videos_list li').eq(n-1).find('h3').text();
-			var vid_car_title = $('<h5>');
-			vid_car_title.html(vid_car_title_data);
-			vid_car_title.appendTo(vid_car_item);
+			vid_car_item += '<h5>' + vid_car_title_data + '</h5>';
+			vid_car_item += '</div>';
 			owl_vids_car.trigger('add.owl.carousel', [vid_car_item, 0]);
 		}
 		owl_vids_car.trigger('refresh.owl.carousel');
@@ -121,17 +76,20 @@ $(document).ready(function(){
 				'height' : $('#videos_carousel').height() + 'px'
 			});
 			
-			
-			var srcUrl = $(this).find('img').attr('src');
-			var imgContainer = $(this);
-			var imgIndex = $(this).index();
-			//console.log(imgIndex);
 			var imgRegex = /https:\/\/i3.ytimg.com\/vi\/([\w-]{11})\/mqdefault.jpg/;
 			//get the videoid from each li item
 			var vidImgDat = $(this).find('img').attr('src');
 			var vidId = vidImgDat.match(imgRegex)[1];
 			//play the video on the player
 			$(this).on("click", "img", function(){
+				$("html, body").animate({
+					scrollTop: $("#weeks_carousel_container").offset().top 
+				},500);
+				player.loadVideoById(vidId);
+			}).on("click", "h5", function(){
+				$("html, body").animate({
+					scrollTop: $("#weeks_carousel_container").offset().top 
+				},500);
 				player.loadVideoById(vidId);
 			});
 		});
